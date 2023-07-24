69ee224b4be7ff861ecd40dde3baaea447b72cb1
Shaya Ahmed <shayaahmed06@gmail.com>
2023-07-23 22:25:55 -0400

    updated commit

--- 04-addTwoNums.js
+++ 04-addTwoNums.js
@@ -1,51 +1,66 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/*
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
+//  Definition for singly-linked list.
+function ListNode(val, next) { 
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
 
-var addTwoNumbers = function(l1, l2) {
-    //step 6: create a variable that stores the number that gets carried over and set to 0
-    let carry = 0;
-    //step 10: create a variable that will keep track of the previous node
-    // let PreviousNode = new addTwoNumbers();  //initialize it to an empty node
-    // let headNode = PreviousNode;
-    //step 1: I want to traverse the link list using a while loop
-    while(l1 || l2 || carry){ //step 2: traverse both link list to the very end, the loop needs to keep running while l1 or l2 is not null
-        //step 4: create a variable where it stores the value
-        let val1 = l1 ? l1.val : 0; // terary operator, if l1 is not null set val1 to be val1.val else set it to be 0
-        let val2 = l2 ? l2.val : 0;
-        //step 3: since l1 and l2 can have different lengths we need an if statement
-        //if l1 is not equal to null
-        if(l1){
-            //set l1 to be the next node
-            l1 = l1.next;
-        }
-        //if l2 is not equal to null
-        if(l2){
-            //set l2 to be the next node
-            l2 = l2.next;
-        }
-        //step 5: create a variable where it stores the sum of both of the lists
-        let sum = val1 + val2 + carry;
-        //step 7: set carry to be the number getting carried over
-        carry = Math.floor(sum/10) // sum > 9 ? 1 : 0;
-        // //step 8: create a variable that stores the leftover of the sum Ex. 14 % 10 = 4. 
-        let digit = sum % 10;
-        console.log(digit);
-        // //step 9: create a li3nked list to return
-        // let currNode = new ListNode(digit);
-        // PreviousNode.next = currNode;
-        // PreviousNode = currNode;
+const toLinked_L1 = (l1) => {
+    let curr= new ListNode(0);
+    let p = curr;
+    for(const i of l1){
+        curr.next = new ListNode(i);
+        curr = curr.next;
     }
-    // return headNode.next;
+    // addTwoNumbers(p1.next, p2.next);
+    return p.next;
+}
+const toLinked_L2 = (l2) => {
+    let curr = new ListNode(0);
+    let p = curr;
+    for(const i of l2){
+        curr.next = new ListNode(i);
+        curr = curr.next;
+    }
+    // addTwoNumbers(p1.next, p2.next);
+    return p.next;
+}
+
+const addTwoNumbers = (l1, l2) => {
+    let carry = 0,
+        result = new ListNode(0),
+        current = result;
+        arr = [];
+
+    //step 1: I want to traverse the link list using a while loop
+    while(l1 || l2 || carry){ 
+
+        // terary operator, if l1 is not null set val1 to be val1.val else set it to be 0
+        let val1 = l1 ? l1.val : 0, 
+            val2 = l2 ? l2.val : 0;
+
+        if(val1 + val2 + carry > 9){
+            current.next = new ListNode(val1 + val2 + carry - 10);
+            current = current.next;
+            // console.log(val1 + val2 + carry - 10);
+            carry = 1;
+        }else{
+            current.next = new ListNode(val1 + val2 + carry);
+            current = current.next;
+            // console.log(val1 + val2 + carry);
+            carry = 0;
+        }
+        //if l1, l2 is not equal to null set them to be the next node.
+        if(l1) l1 = l1.next;
+        if(l2) l2 = l2.next;
+    }
+    if(carry) current.next = new ListNode(carry);
+    // return result.next.val;
+    while(result){
+        arr.push(result.val);
+        result = result.next;
+    }
+    return arr;
 };
 
-console.log(addTwoNumbers([9,9], [1,6]))
+
+console.log(addTwoNumbers(toLinked_L1([2,4,3]), toLinked_L2([5,6,4])));
